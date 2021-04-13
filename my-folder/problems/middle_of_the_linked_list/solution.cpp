b6bdc96eb209d357d6ba9c75631f98e916054a11
and9527 <and95277@gmail.com>
2021-04-13 12:53:25 +0000

    Sync LeetCode submission - Middle of the Linked List (cpp)

---
+++ my-folder/problems/middle_of_the_linked_list/solution.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *p = head;
+        int counter = 0;         
+        while(p != NULL)
+        {
+            p = p->next;
+            counter++;
+        }    
+        counter /= 2;  
+        while(counter-- > 0)
+        {
+            head = head->next;
+            // printf("%d\n", &head);
+        }
+        return head;
+    }
+};
