34eb016e4947ec4082d68a9e8c08043d74ea4a4a
and9527 <and95277@gmail.com>
2022-08-25 16:13:11 +0000

    Sync LeetCode submission - Network Delay Time (cpp)

--- my-folder/problems/network_delay_time/solution.cpp
+++ my-folder/problems/network_delay_time/solution.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        //建圖,存在出邊陣列
+        ver = vector<vector<int>>(n+1,vector<int>());
+        edge = vector<vector<int>>(n+1,vector<int>());
+        for (vector<int>& t :times){
+            int x = t[0];
+            int y = t[1];
+            int z = t[2];
+            ver[x].push_back(y);
+            edge[x].push_back(z);
+        }
+        
+        //Dijkstra 算法
         dist = vector<int>(n+1,1e9);
         dist[k] = 0;
-        
-        for (int round = 1;round<=n-1;round++){
-        bool flag = false;
-            for(vector<int>&edge :times){
-            int x = edge[0];
-            int y = edge[1];
-            int z = edge[2];
-                if(dist[x]+z < dist[y]){
-                    dist[y] = dist[x]+z;
-                    flag = true;
-                }                
-            }  
-            if(!flag) break;
+        expand =vector<bool>(n+1,false);
+        q.push({0,k});
+        while (!q.empty()){
+            int x=q.top().second;
+            q.pop();
+            if(expand[x]) continue;
+            expand[x] = true;
+            for (int i = 0 ; i<ver[x].size();i++){
+                int y= ver[x][i];
+                int z= edge[x][i];
+                if(dist[y]> dist[x]+z){
+                    dist[y]= dist[x]+z;
+                    q.push({-dist[y],y});
+                }
+            }
         }
         int ans =0;
         for(int i=1;i<=n;i++) ans = max(ans,dist[i]);
@@ -23,4 +37,9 @@ public:
     }
 private:
     vector<int> dist;
+    vector<bool> expand;
+    vector<vector<int>> ver;
+    vector<vector<int>> edge;
+    // <距離 編號>
+    priority_queue<pair<int,int>> q;
 };
